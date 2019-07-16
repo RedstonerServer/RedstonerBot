73e632044a549c2490e5be936bc9683eb272a34a
David Panić <david@panic.tk>
2019-07-16 20:26:36 +0200

    Initial commit

---
+++ src/main/java/com/redstoner/redstonerBot/Main.java
@@ -0,0 +1,12 @@
+package com.redstoner.redstonerBot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+	public static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+	public static void main(final String... args) {
+		logger.info("Starting RedstonerBot...");
+	}
+}
